a072e37d76b62d3729b0c93733d0922ea99bfde3
Muhammad Hasnain <xmarthasnain424@gmail.com>
2024-09-10 13:36:31 +0500

    percentage program

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+let per = Number(prompt("Enter Number"));
+if(per>50 && per<60)
+{
+    document.write("C Grade");
+    
+}
+else if (per>60 && per<=70)  
+{
+    document.write("B Grade");
+}
+else if (per>70 && per<=80)
+    {
+        document.write("A Grade");
+    }
+    else if (per>80 && per<=100)
+        {
+            document.write("A1 Grade");
+        }
+else 
+{
+    document.write("Fail");
+    
+}
+
